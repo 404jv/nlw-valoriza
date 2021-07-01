a24b82c82d31459293af538253d696c21a5e5a6b
João Victor <joaovictorramalho7@gmail.com>
2021-07-01 19:20:28 -0300

    Validação de dados

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -1,12 +1,30 @@
 import { Request, Response } from "express";
-import { CREATED } from "../helpers/httpHelpers";
+import { BAD_REQUEST, CREATED } from "../helpers/httpHelpers";
 import { CreateUserService } from "../services/CreateUserService";
+import * as yup from 'yup';
+import { HttpError } from "../errors/HttpError";
 
 
 class CreateUserController {
   async handle(request: Request, response: Response) {
+    const schema = yup.object().shape({
+      name: yup.string().required(), 
+      email: yup.string().email().required(), 
+      admin: yup.boolean().notRequired(), 
+      password: yup.string().required() 
+    });
+  
+    try {
+      schema.validateSync(request.body);
+    } catch (error) {
+      throw new HttpError({ 
+        message: error.errors, 
+        statusCode: BAD_REQUEST
+      });
+    }
+
     const { name, email, admin, password } = request.body;
-    
+
     const createUserService = new CreateUserService();
 
     const user = await createUserService.execute({ name, email, admin, password });
